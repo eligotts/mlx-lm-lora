52fc4c6d99358a862d6ac2c4ead22ba688c9da44
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2025-05-11 15:41:38 +0200

    Update requirements.txt to include setuptools version constraint and ensure datasets is listed. Modify setup.py to restrict Python version compatibility to <3.12.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license="MIT",
     install_requires=requirements,
     packages=["mlx_lm_lora", "mlx_lm_lora.trainer"],
-    python_requires=">=3.8",
+    python_requires=">=3.8, <3.12",
     extras_require={},
     entry_points={
         "console_scripts": [
